bdd6c3fd6ff5b5fc491400695c435bb51b55c94e
unknown <UFG@CC5-13.ufg.local>
2023-09-30 12:10:17 -0600

    cuarto commit

--- Parcialito2/Models/VistaResultadosElecciones.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Parcialito2.Models
-{
-    public class VistaResultadosElecciones
-    {
-        public string Candidato { get; set; }
-        public int CantidadDeVotos { get; set; }
-        public decimal Porcentaje { get; set; }
-    }
-}
---
+++ Parcialito2_v2/Models/VistaResultadosEleccione.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parcialito2_v2.Models;
+
+public partial class VistaResultadosEleccione
+{
+    public string? Candidato { get; set; }
+
+    public int? CantidadDeVotos { get; set; }
+
+    public decimal? Porcentaje { get; set; }
+}
